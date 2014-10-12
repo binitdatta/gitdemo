239b14535f0f75725d4cbea109380911ba16ad33
Binit Datta <binitdatta@gmail.com>
2014-10-12 15:08:54 -0500

    first commit

---
+++ src/main/java/com/rollingstone/gitdemo/App.java
@@ -0,0 +1,13 @@
+package com.rollingstone.gitdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
